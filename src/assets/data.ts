baeac8517a5fadbbedc37f22f15c369a32b34833
DREW TECHIE <Apple@DREW-TECHIE.local>
2026-03-07 08:25:13 +0100

    feat: add images

---
+++ src/assets/data.ts
@@ -0,0 +1,42 @@
+// Static imports for Vite asset bundling
+
+import img1 from './631349802_18551109130006129_8653852994271188690_n.jpg';
+import img2 from './631684769_18551109160006129_179417945371232936_n.jpg';
+import img3 from './498193390_18406337332100976_6220192687240442961_n.webp';
+import img4 from './499268712_18406338277100976_1244696641256165189_n.webp';
+import img5 from './499275479_18406338346100976_4537061319686658638_n.webp';
+import img6 from './499480675_18406338295100976_5569329076239804991_n.webp';
+import img7 from './500063357_18406613812100976_4608311866107652165_n.webp';
+import img8 from './500578134_18407341429100976_8670122004350935656_n.webp';
+import img9 from './587016891_18435550237100976_1207277761193894304_n.webp';
+
+// We also have videos, we can categorize them if needed
+import vid1 from './AQMlOGAWv8cMgVTS4vqieD0-2_RYbefQ-fqoMrVOLVQQ3CIBiSw4diIftUJjoerd2zY0CZywos2gEQywVpeQD_1PG3nRdJl2ONvo8ec.mp4';
+import vid2 from './AQMtsCE6BN4Nb764vNCrYjYSXsS0dlxLKyGMgPMSOmIuB6pZFdEBFFq6ROYWHBYFlFETamBCIscL2M0cgeJDB1kDLIyvHwQ7qH-PWeQ.mp4';
+import vid3 from './AQNrrGnwxpi98GPks9khCU2C-gkVGELPFmOEl_xdj1QDx-KMUSET2K0qYjbf1wb0G4ReGjTik7JYNCTaMq8qgQSA.mp4';
+
+// For the Loader flash (needs to be images)
+export const loaderImages = [
+  img1, 
+  img2, 
+  img3, 
+  img4
+];
+
+// For the Art Dir scrolling gallery (can be images or video posters)
+export const galleryImages = [
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img1,
+  img2
+];
+
+// For future use if a video hero is needed
+export const heroVideos = [
+  vid1,
+  vid2,
+  vid3
+];
